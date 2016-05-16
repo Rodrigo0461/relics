859408764a95249166a8d3f214560790e731eee7
Rodrigo Pichiñual <rodrigo.pichinual@gmail.com>
2016-05-16 18:11:55 -0400

    beginning with table display financiador

---
+++ application/views/financiador/financiador_list.php
@@ -0,0 +1,26 @@
+<!-- Se despliega la tabla despues de presionar el boton -->
+<section class="res-sec" style="display: none;">				
+    <div id="container_in">
+        <div class="et-inner">
+            <h1 class="et-left clr" id="search-head-div"><strong>Financiadores</strong></h1>
+            <div id="infoMessage" class="et-left toppadd"></div>
+            <div class="btmpadd"><br></div>
+            <!-- tabla donde se muestran los resultados -->
+            <table cellpadding=0 cellspacing=0 id="result_table" class="toppadd1">
+                <thead>
+                    <tr>
+                        <th>Financiador</th>
+                        <th>Cod Financiador</th>
+                        
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+            
+        </div>
+    </div>   
+</section>
+
+
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/financiador_table.js"></script> 
+
---
+++ assets/js/financiador_table.js
@@ -0,0 +1,53 @@
+var oTable, quote_numbr, rut_client, client_name, user_id, from_date, to_date;
+$(function() {
+    
+    $("#date_search").click(function() {
+        rut_client   = $("#financiador").val();
+        $(".res-sec").css("display","");
+        oTable.fnDraw();
+        //$("#search-form-div").css("display","none");
+         
+   });
+   
+    oTable = $("#result_table").dataTable({
+        "sDom": "<'row et-cf toppad btmpadd'<'span6'l><'span6'f><'span6'r>>t<'row'<'span6'i><'span6'p>>",
+        "bProcessing": true,
+        "bServerSide": true,
+        "bPaginate": true,
+        "sPaginationType": "full_numbers",
+        "bLengthChange": true,
+        "bFilter": true,
+        "bSort": true,
+        "iDisplayLength": 10,
+        "fnServerData": function ( sSource, aoData, fnCallback ) {
+            aoData.push( { "name" : "financiador", "value" :  financiador });
+            $.getJSON( sSource, aoData, function (json) { 
+                fnCallback(json);
+
+            });
+        },
+        "sAjaxSource": "home/financiador_table",
+        "aLengthMenu": [[10, 25, 50, -1], [10, 25, 50, "Todo"]],
+	"aaSorting": [[1, 'ASC']],
+        "bAutoWidth": false,
+        "aoColumns": [
+            {"sWidth": "10%", "bVisible": true, "bSearchable": true, "bSortable": true},
+            {"sWidth": "50%", "bVisible": true, "bSearchable": false, "bSortable": true},
+            {"sWidth": "10%", "bVisible": true, "bSearchable": false, "bSortable": false},
+            {"sWidth": "10%", "bVisible": true, "bSearchable": false, "bSortable": false},
+            {"sWidth": "10%", "bVisible": true, "bSearchable": false, "bSortable": false},
+            {"sWidth": "10%", "bVisible": true, "bSearchable": false, "bSortable": false},
+        ],
+        "oLanguage": {
+            "sLengthMenu": "Mostrar _MENU_ entradas",
+            "sZeroRecords": "No se encontraron registros",
+            "sInfo": "Mostrando _START_ a _END_ de registros _TOTAL_",
+            "sInfoEmpty": "Mostrando 0 a 0 de registros 0",
+            "sInfoFiltered": "(filtrado de los registros totales _MAX_)",
+
+        }
+    });
+    
+});  
+
+
