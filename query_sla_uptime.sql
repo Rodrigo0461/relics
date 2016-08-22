aa9c547105cb28a87908620aeaf83e3f6955af83
Rodrigo0461 <rodrigo.pichinual@gmail.com>
2016-08-22 00:02:02 -0300

    QUERYS FOR PRESTADORES

--- query_sla_uptime.sql
+++ query_sla_uptime.sql
@@ -16,45 +16,11 @@ AND   s.Dayxweek=4 AND u.timestamp >='2016-07-17'
 AND u.timestamp<='2016-07-19';
 
 
-mysql> describe SLA112014;
-+-----------------+--------------+------+-----+---------+----------------+
-| Field           | Type         | Null | Key | Default | Extra          |
-+-----------------+--------------+------+-----+---------+----------------+
-| Week            | int(10)      | YES  |     | NULL    |                |
-| Dayxweek        | int(10)      | YES  |     | NULL    |                |
-| Hour            | varchar(10)  | YES  |     | NULL    |                |
-| Minute          | varchar(10)  | YES  |     | NULL    |                |
-| BonosResBonos   | int(100)     | YES  |     | NULL    |                |
-| SRBMTotal       | int(100)     | YES  |     | NULL    |                |
-| SRBAporteFinan  | int(100)     | YES  |     | NULL    |                |
-| SRBAporteSeg    | int(100)     | YES  |     | NULL    |                |
-| SRBCopagoBenef  | int(100)     | YES  |     | NULL    |                |
-| NameFinanciador | varchar(100) | YES  |     | NULL    |                |
-| NamePrestador   | varchar(100) | YES  |     | NULL    |                |
-| id              | int(11)      | NO   | PRI | NULL    | auto_increment |
-| time            | time         | NO   |     | NULL    |                |
-+-----------------+--------------+------+-----+---------+----------------+
-13 rows in set (0.00 sec)
-
-
-
-
-
-
- 
-mysql> describe uptime;
-+-----------------+------------------+------+-----+-------------------+-----------------------------+
-| Field           | Type             | Null | Key | Default           | Extra                       |
-+-----------------+------------------+------+-----+-------------------+-----------------------------+
-| id              | int(10) unsigned | NO   | PRI | NULL              | auto_increment              |
-| cod_financiador | int(10)          | NO   |     | NULL              |                             |
-| financiador     | varchar(50)      | NO   |     | NULL              |                             |
-| timestamp       | timestamp        | NO   |     | CURRENT_TIMESTAMP | on update CURRENT_TIMESTAMP |
-| estado          | tinyint(1)       | YES  |     | NULL              |                             |
-| Bono3           | tinyint(1)       | NO   |     | 0                 |                             |
-+-----------------+------------------+------+-----+-------------------+-----------------------------+
-6 rows in set (0.00 sec)
-
-
-
-
+SELECT u.id,s.id,s.NamePrestador, s.NameFinanciador,s.time, s.Dayxweek  ,u.cod_financiador, u.financiador, u.timestamp
+FROM SLA112014 s INNER JOIN uptime u
+WHERE s.NamePrestador='Servicios De Salud Integrados' 
+AND s.NameFinanciador='Colmena' 
+AND s.NameFinanciador=u.financiador 
+AND s.Dayxweek=4 AND u.timestamp > '2016-08-18 21:12:00'
+AND s.time > '21:26:00' AND time < '21:40:00' AND u.estado=0
+GROUP BY u.id;
